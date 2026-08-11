5ae17a952e9d5dd94fc1662a43e3c3c279b720f1
Kyle Richter <kylerichter36-coder@users.noreply.github.com>
2026-08-11 12:58:50 +0200

    Auto-retrain ML Model v14 [12:58:50]

--- home_sender/grid_model.h
+++ home_sender/grid_model.h
@@ -1,20 +1,20 @@
 // Auto-generated GridWatcher ML Model Decision Weights
-// Retrained on: 2026-08-11 12:48:43
-// Accuracy: 100.00%
+// Retrained on: 2026-08-11 12:57:38
+// Accuracy: 98.36%
 
 #ifndef GRID_MODEL_H
 #define GRID_MODEL_H
 
 #ifndef ML_MODEL_VERSION
-#define ML_MODEL_VERSION 13
+#define ML_MODEL_VERSION 14
 #endif
 
 #ifndef ML_MODEL_BUILD_TIME
-#define ML_MODEL_BUILD_TIME "2026-08-11 12:48:43"
+#define ML_MODEL_BUILD_TIME "2026-08-11 12:57:38"
 #endif
 
 #ifndef ML_MODEL_ACCURACY
-#define ML_MODEL_ACCURACY "100.00%"
+#define ML_MODEL_ACCURACY "98.36%"
 #endif
 
 inline float predictGridRisk(float voltage, float frequency, float signal) {
