3c80f2b87a914abdf66216234913ddfde1450c16
Kyle Richter <kylerichter36-coder@users.noreply.github.com>
2026-08-11 09:38:31 +0200

    Auto-retrain ML Model v8 [09:38:30]

--- gridwatcher_dashboard2/gridwatcher_dashboard2.ino
+++ gridwatcher_dashboard2/gridwatcher_dashboard2.ino
@@ -804,6 +804,7 @@ void loop() {
       int cellIdx= str.indexOf(",S:");
       int pbIdx  = str.indexOf(",PB:");
       int sIdx   = str.indexOf(",SEQ:");
+      int mlIdx  = str.indexOf(",ML:");
 
       if (vIdx >= 0 && fIdx > vIdx && sIdx > fIdx) {
         packetsReceivedThisSecond++;
@@ -824,7 +825,13 @@ void loop() {
           phoneBatteryPercent = str.substring(pbIdx + 4, sIdx).toInt();
         }
 
-        unsigned long seq = str.substring(sIdx + 5).toInt();
+        // Parse ML version from packet (live update from Base Station)
+        if (mlIdx > sIdx) {
+          int newML = str.substring(mlIdx + 4).toInt();
+          if (newML > 0) mlVersion = newML;
+        }
+
+        unsigned long seq = str.substring(sIdx + 5, mlIdx > sIdx ? mlIdx : str.length()).toInt();
         if (lastExpectedSeq != 0 && seq > lastExpectedSeq) {
           totalLostPackets += (seq - lastExpectedSeq);
         }
